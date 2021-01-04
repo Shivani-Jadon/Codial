0bbd9ad0eeee84e38e6d36b264ab8fe158576668
Shivani-Jadon <shivanijadon98@gmail.com>
2021-01-04 19:40:35 +0530

    add Nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,41 @@ class App extends React.Component{
     const { posts }  = this.props;
     return (
       <div>
+        <nav className='nav'>
+          <div className='left-div'>
+            {/* <img className='logo-icons' src="https://www.flaticon.com/svg/static/icons/svg/3437/3437338.svg" alt="logo" />Codial */}
+          </div>
+
+          <div className='search-container'>
+            <input placeholder="Search" />
+            <div className="search-results">
+              <ul>
+                <li className="search-results-row">
+                  <img  src="https://www.flaticon.com/svg/static/icons/svg/3237/3237447.svg" alt="User Avatar"/>
+                  <span>Shivani J</span>
+                </li>
+                <li className="search-results-row">
+                  <img  src="https://www.flaticon.com/svg/static/icons/svg/3237/3237447.svg" alt="User Avatar"/>
+                  <span>Shivani J</span>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+          <div className='right-nav'>
+            <div className='user'>
+                <img src="https://www.flaticon.com/svg/static/icons/svg/3237/3237447.svg" alt="User Avatar" id="user-dp"/>
+                <span>Shivani J</span>
+            </div>
+            <div className="nav-links">
+              <ul>
+                <li>Log in</li>
+                <li>Log out</li>
+                <li>Register</li>
+              </ul>
+            </div>
+          </div>
+        </nav>
         <PostList posts={posts} />
       </div>
       
