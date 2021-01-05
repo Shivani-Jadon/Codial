9100b63995b4176a75e390190e201781b18b5755
Shivani-Jadon <shivanijadon98@gmail.com>
2021-01-05 17:51:44 +0530

    handle login form with controlled(React) components

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,14 +4,34 @@ class Login extends React.Component{
 
     constructor(props){
         super(props);
-        this.emailInputRef = React.createRef();
-        this.passwordInputRef  = React.createRef();
+        // form handling with uncontrolled(Dom) components
+        // this.emailInputRef = React.createRef();
+        // this.passwordInputRef  = React.createRef();
+
+        // form handling with controlled(React) components
+        this.state = {
+            email : '',
+            password : '',
+        }
+    }
+
+    handleEmailChange = (e) => {
+        this.setState({
+            email: e.target.value,
+        })
+    }
+
+    handlePasswordChange = (e) => {
+        this.setState({
+            password: e.target.value,
+        })
     }
 
     handleFormSubmit = (event) => {
         event.preventDefault();
-        console.log("emailRef", this.emailInputRef.current.value);
-        console.log("passwordRef", this.passwordInputRef);
+        // console.log("emailRef", this.emailInputRef.current.value);
+        // console.log("passwordRef", this.passwordInputRef);
+        console.log(this.state);
     }
 
     render(){
@@ -20,10 +40,16 @@ class Login extends React.Component{
             <form className='login-form'>
                 <span className='login-signup-header'>Login</span>
                 <div className='field'>
-                    <input type="email" placeholder="Email" required ref={this.emailInputRef} />
+                    <input type="email" placeholder="Email" required 
+                    // ref={this.emailInputRef} 
+                    onChange={this.handleEmailChange}
+                    />
                 </div>
                 <div className='field'>
-                    <input type="password" placeholder="Password" required ref={this.passwordInputRef} />
+                    <input type="password" placeholder="Password" required 
+                    // ref={this.passwordInputRef} 
+                    onChange={this.handlePasswordChange}
+                    />
                 </div>
                 <div className='field'>
                     <button onClick={this.handleFormSubmit}>Login</button>
