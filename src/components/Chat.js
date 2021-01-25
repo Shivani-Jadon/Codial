fb2fb18a70837e5c543650c3d459ea4b5b6ab2a5
Shivani-Jadon <shivanijadon98@gmail.com>
2021-01-25 17:27:10 +0530

    add web sockets for chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
+import {connect} from 'react-redux';
 
 class Chat extends Component {
     constructor (props) {
@@ -7,6 +9,59 @@ class Chat extends Component {
         this.state = {
             messages : [],
             typedMessage: '',
+        }
+
+        this.socket = io.connect('http://54.237.58.65:5000');
+        this.userEmail = props.user.email;
+
+        if (this.userEmail) {
+            this.setupConnections();
+        }
+
+    }
+    
+    setupConnections = () => {
+        const socketConnection = this.socket;
+
+        this.socket.on('connect', function() {
+            console.log("Connetion established");
+
+            socketConnection.emit('join_room', {
+                user_email: this.userEmail,
+                chatroom: 'codial',
+            });
+
+            socketConnection.on('user_joined', function (data) {
+                console.log("New user joined", data);
+            })
+        })
+
+        this.socket.on('receive_message', function (data) {
+            // add mesasge to state
+            // const {messages} = self.state;
+            // const messageObject = {};
+            // messageObject.content = data.message;
+
+            // if(data.user_email === self.userEmail) {
+            //     messageObject.self = true;
+            // }
+
+            // self.setState({
+            //     messages: [...messages, messageObject],
+            //     typedMessage: '',
+            // });
+        });
+    }
+    
+    handleSubmit = () => {
+        const { typedMessage } = this.state;
+
+        if (typedMessage && this.userEmail) {
+            this.socket.emit('send_message', {
+                message: typedMessage,
+                user_email: this.userEmail,
+                chatroom: 'codial',
+            });
         }
     }
 
@@ -33,11 +88,17 @@ class Chat extends Component {
                 <div className="chat-footer">
                     <input type="text" value={typedMessage}
                         onChange={(e) => this.setState({ typedMessage: e.target.value})} />
-                    <button></button>
+                    <button>Submit</button>
                 </div>
             </div>
         );
     }
 }
 
-export default Chat;
+function mapStateToProps({auth}) {
+    return {
+        user: auth.user,
+    }
+}
+
+export default connect(mapStateToProps)(Chat);
